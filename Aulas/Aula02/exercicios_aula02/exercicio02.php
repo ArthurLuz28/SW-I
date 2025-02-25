1848ed247da0b0c42b3114f690affbe9eed65e31
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2025-02-25 19:01:07 -0300

    Add files via upload

---
+++ Aulas/Aula02/exercicios_aula02/exercicio02.php
@@ -0,0 +1,13 @@
+<?php
+
+    define("Pi", 3.141592);
+
+    $r = 2;
+
+    $AREA = 4 * Pi * $r**2;
+    $VOLUME = 4/3 * Pi * $r**3;
+
+    echo "Área: $AREA <br>";
+    echo "Volume: $VOLUME";
+
+?>
---
+++ Aulas/Aula02/exercicios_aula02/exercicio03.php
@@ -0,0 +1,15 @@
+<?php
+
+    $n1 = 20;
+    $n2 = 13;
+
+    $soma = $n1 + $n2;
+    $sub = $n1 - $n2;
+    $mult = $n1 * $n2;
+    $rest = $n1 % $n2;
+
+    echo "Soma: $soma <br>";
+    echo "Subtração: $sub <br>";
+    echo "Multiplicação: $mult <br>";
+    echo "Resto de divisão: $rest";
+?>
