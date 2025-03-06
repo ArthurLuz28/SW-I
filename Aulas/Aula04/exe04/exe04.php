0048eaf004cef16f3503466d6091e2bef807adc3
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2025-03-06 20:22:54 -0300

    Update exe04.php

--- Aulas/Aula04/exe04/exe04.php
+++ Aulas/Aula04/exe04/exe04.php
@@ -3,5 +3,11 @@
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $data = $_POST["data"];
+    $cartao = $_POST["cartoes"];
 
-    
+    echo "Olá, seu nome é: $nome <br>";
+    echo "Seu email: $email <br>";
+    echo "Sua data de nascimento: $data <br>";
+    echo "Seu cartão de crédito: $cartao";
+
+?>
