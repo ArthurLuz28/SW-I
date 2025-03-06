53b511c155c5a4e809d6b4f78414d73ffd975abc
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2025-03-06 20:23:15 -0300

    Update form04.php

--- Aulas/Aula04/exe04/form04.php
+++ Aulas/Aula04/exe04/form04.php
@@ -10,6 +10,13 @@
         <p>Nome: <input type="Text" name="nome" required> </p>
         <p>Email: <input type="Email" name="email" required> </p>
         <p>Data de Nascimento: <input type="Date" name="data" required> </p>
+        <p>Escolha um cartão de crédito: </p>
+        <select name="cartoes">
+        <option value="nubank">Nubank</option>
+        <option value="magalu">Itaú Magalu</option>
+        <option value="pago">Mercado Pago</option>
+        <option value="carrefour">Carrefour</option>
+        </select>
         <input type="submit" value="Submit">
 </body>
 </html>
