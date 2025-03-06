7e39340edffa68a384efe5adbee96782f1c886d8
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2025-03-06 20:23:58 -0300

    Add files via upload

---
+++ Aulas/Aula04/exe05/exe05.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5 Aula 4 - Post</title>
+</head>
+<body style="background-color: <?php echo $_Po["cor"]; ?>" >
+    
+</body>
+</html>
---
+++ Aulas/Aula04/exe05/form05.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5 Aula 4 - Post</title>
+</head>
+<body>
+    <form action="exe05.php" method="POST">
+        <p>Escolha uma cor:</p>
+        <input type="color" name="cor" value="#ff0000" required>
+        <input type="submit" value="Submit">
+</body>
+</html>
