575445b9c6788176dd331dafaef927b390c1b51c
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2025-06-11 12:44:24 -0300

    Add files via upload

---
+++ Aulas/aula_13/exercicio03/exercicio03.php
@@ -0,0 +1,7 @@
+<?php
+
+    include_once 'Funcionario.class.php';
+
+    $funcionario = new Funcionario ("Eu mesmo", 2);
+    $funcionario->aumentarSalario(100);
+    $funcionario->exibirInformacoes();
---
+++ Aulas/aula_13/exercicio04/Retangulo.class.php
@@ -0,0 +1,19 @@
+<?php
+
+    class Retangulo {
+        private $Largura;
+        private $Altura;
+
+        public function __construct($larg, $alt) {
+            $this->Largura = $larg;
+            $this->Altura = $alt;
+        }
+
+        public function calcularArea() {
+
+        }
+
+        public function calcularPerimetro() {
+            
+        }
+    }
