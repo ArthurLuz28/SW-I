97c2589f8bc2fdd94f9373184be574e6acee8935
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2025-06-11 18:30:22 -0300

    Delete Aulas/aula_13 directory

--- Aulas/aula_13/exercicio03/exercicio03.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include_once 'Funcionario.class.php';
-
-    $funcionario = new Funcionario ("Eu mesmo", 2);
-    $funcionario->aumentarSalario(100);
-    $funcionario->exibirInformacoes();
